5d313379acce8d055883dcb22a90420abcd8ca04
akazawaya <ktakimotondsinc@gmail.com>
2025-09-30 06:46:19 +0900

    シリアライザにget_foo_displayの表記を追加

--- project/app_ocr/serializers.py
+++ project/app_ocr/serializers.py
@@ -2,22 +2,28 @@ from rest_framework import serializers
 from app_ocr.models import Document, Page, Content
 from django.contrib.auth.models import User
 
+# http -f POST http://localhost:8000/documents/ file@"/mnt/..." file_name=test file_type=0
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'id', 'username', 'snippets']
+        fields = ['url', 'id', 'username']
 
 class DocumentSerializer(serializers.HyperlinkedModelSerializer):
+    state_display = serializers.CharField(source="get_state_display", read_only=True)
+    file_type_display = serializers.CharField(source="get_file_type_display", read_only=True)
     class Meta:
         model = Document
-        fields = ['created', 'state', 'file', 'file_name', 'file_type', 'total_pages']
+        fields = ['id', 'state', 'state_display', 'created', 'file', 'file_name', 'file_type', 'file_type_display', 'total_pages']
 
 class PageSerializer(serializers.HyperlinkedModelSerializer):
+    state_display = serializers.CharField(source="get_state_display", read_only=True)
     class Meta:
         model = Page
-        fields = ['doc', 'image', 'state', 'page_number', 'width_px', 'height_px']
+        fields = ['doc', 'image', 'state', 'state_display', 'page_number', 'width_px', 'height_px']
 
 class ContentSerializer(serializers.HyperlinkedModelSerializer):
+    direction_display = serializers.CharField(source="get_direction_display", read_only=True)
     class Meta:
         model = Content
-        fields = ['page', 'bbox', 'content', 'direction', 'rec_score', 'det_score']
+        fields = ['page', 'bbox', 'content', 'direction', 'direction_display', 'rec_score', 'det_score']
