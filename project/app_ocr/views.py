8a7a9901a450afc6f1b125c0080701ee44fd5dcb
akazawaya <ktakimotondsinc@gmail.com>
2025-10-01 06:31:58 +0900

    カスタムアクションanalyze（OCRの下処理）を実装

--- project/app_ocr/views.py
+++ project/app_ocr/views.py
@@ -14,16 +14,36 @@ from rest_framework import permissions
 from rest_framework import renderers
 from rest_framework.decorators import action
 from rest_framework import viewsets
+from rest_framework import status
+from app_ocr.etc.tools import validate_and_analyze
+from rest_framework.parsers import MultiPartParser, FormParser
 
+"""
+http -f POST http://127.0.0.1:8000/documents/analyze/      file@"/Users/XXXXX.png" Accept:application/json
+"""
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 class DocumentViewSet(viewsets.ModelViewSet):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    @action(detail=False, methods=['post'])
+    def analyze(self, request):
+        file = request.FILES.get('file')   
+        images, ext, total_pages = validate_and_analyze(file)
+        doc = DocumentSerializer(data={"file":file, "file_name":file.name, "file_type":ext, "total_pages":total_pages})
+        if doc.is_valid():
+            doc.save()
+            return Response(doc.data, status=status.HTTP_201_CREATED)
+        
+        return Response(doc.errors, status=status.HTTP_400_BAD_REQUEST)
 class PageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Page.objects.all()
     serializer_class = PageSerializer
+
 class ContentViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
