8b5fad77bbfabbacd9d35eb75dfe668dbdcbc7d1
akazawaya <ktakimotondsinc@gmail.com>
2025-10-07 19:20:41 +0900

    ocrメゾット本体を作成

--- project/app_ocr/views.py
+++ project/app_ocr/views.py
@@ -1,23 +1,22 @@
 from django.contrib.auth.models import User
-from app_ocr.models import FileType, Document, Page, Content
+from app_ocr.models import FileType, Direction, Document, Page, Content
 
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from django.core.files.base import ContentFile
 from app_ocr.serializers import UserSerializer, DocumentSerializer, PageSerializer, ContentSerializer
 
 #from app_ocr.permissions import IsOwnerOrReadOnly
-from rest_framework.reverse import reverse
-
-from rest_framework import permissions
-from rest_framework import renderers
 from rest_framework.decorators import action
 from rest_framework import viewsets
 from rest_framework import status
 from app_ocr.etc.tools import validate_and_analyze
+from app_ocr.etc.e_yomitoku import test_ocr
+
 from rest_framework.parsers import MultiPartParser, FormParser
 from django.db import transaction
+from pathlib import Path
+
 """
 http -f POST http://127.0.0.1:8000/documents/analyze/ file@/mnt/q/N-106.pdf
 """
@@ -39,10 +38,18 @@ class DocumentViewSet(viewsets.ModelViewSet):
             if doc.is_valid(raise_exception=True):
                 doc.save() #　modelインスタンスでないとidを受取れない　doc.instanceでモデルにアクセス可能
                 for p, img in enumerate(images):
-                    img_file = ContentFile(img, name=f"{file}_{p}.png")
+                    img_file = ContentFile(img, name=f"{p}.png")
                     page = PageSerializer(data={"doc":doc.instance.pk, "image":img_file, "page_number":total_pages})
                     if page.is_valid(raise_exception=True):
                         page.save()
+                        # 全体のdirはPath(page.instance.image.name).parent.as_posix()でアクセス可能
+                        page_paths = page.instance.image.name
+                        # breakpoint() 
+                        content_ = test_ocr(page_paths)
+                        for w in content_:
+                            ct = ContentSerializer(data={"page":page.instance.pk, "bbox":w["points"], "content":w["content"], "direction":Direction.label_to_value(w["direction"]), "rec_score":w["rec_score"], "det_score":w["det_score"]})
+                            if ct.is_valid(raise_exception=True):
+                                ct.save()
             return Response({"document": doc.data}, status=status.HTTP_201_CREATED)
                         
         
